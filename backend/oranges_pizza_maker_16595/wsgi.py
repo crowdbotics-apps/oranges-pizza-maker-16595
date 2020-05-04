71d6ba2ee16e1cc8233295485ffa92d3f49fe504
Crowdbotics <team@crowdbotics.com>
2020-05-04 18:24:39 +0000

    Initial commit.

---
+++ backend/oranges_pizza_maker_16595/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for oranges_pizza_maker_16595 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oranges_pizza_maker_16595.settings')
+
+application = get_wsgi_application()
